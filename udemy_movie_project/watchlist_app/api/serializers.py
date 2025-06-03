050989357a32fa779b6e2038a80f999418302c63
Michelle <michelle@Michelles-MacBook-Air.local>
2025-06-02 20:37:14 -0400

    add get and post

--- udemy_movie_project/watchlist_app/api/serializers.py
+++ udemy_movie_project/watchlist_app/api/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-
+from watchlist_app.models import Movie
 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
+    title = serializers.CharField()
     description = serializers.CharField()
     active = serializers.BooleanField()
+
+    # This is the POST method for creating a new movie
+    def create(self, validated_data):
+        # validated data is passed to create method of Movie model
+        return Movie.objects.create(**validated_data) # the ** operator unpacks the dictionary
+    
--- udemy_movie_project/watchlist_app/api/views.py
+++ udemy_movie_project/watchlist_app/api/views.py
@@ -1,13 +1,21 @@
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from watchlist_app.models import Movie
 from watchlist_app.api.serializers import MovieSerializer
 
+@api_view(['GET','POST'])
 def movie_list(request):
-    movie = Movie.objects.all()
-    # Use serializer
-    serializer = MovieSerializer(movie)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        movie = Movie.objects.all()
+        # Use serializer
+        serializer = MovieSerializer(movie, many=True)
+        return Response(serializer.data)
+    else:
+        serializer = MovieSerializer(data=request.data)
 
+
+
+@api_view(['GET','POST'])
 def movie_detail(request, pk):
     movie = Movie.objects.get(pk=pk)
     serializer = MovieSerializer(movie)
