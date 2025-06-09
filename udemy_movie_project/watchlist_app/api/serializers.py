4448aa9a3cd9532a1da70af186e50acf8de3e3f1
Michelle <michelle@Michelles-MacBook-Air.local>
2025-06-09 11:57:47 -0400

    implement ModelSerializer instead of Serializer

--- udemy_movie_project/watchlist_app/api/serializers.py
+++ udemy_movie_project/watchlist_app/api/serializers.py
@@ -1,27 +1,14 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie
+#model serializer
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField()
-    description = serializers.CharField()
-    active = serializers.BooleanField()
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        # fields = '__all__' # fields to be displayed in the response, if not specified, all fields will be displayed
+        # fields = ["id", "title", "description"]
+        exclude = ['active']
 
-    # This is the POST method for creating a new movie
-    def create(self, validated_data):
-        # validated data is passed to create method of Movie model
-        return Movie.objects.create(**validated_data) # the ** operator unpacks the dictionary
-    
-    # This is the PUT menthod for updating an existing movie
-    def update(self, instance, validated_data):
-        # instance is the movie object to be updated, validated_data is the new data
-        instance.title = validated_data.get('title', instance.title)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
-
-    # Field level validation
     def validate_title(self, value):
         if len(value) < 2:
             raise serializers.ValidationError('The title must be at least 2 characters long.')
@@ -32,3 +19,41 @@ class MovieSerializer(serializers.Serializer):
     def validate(self, data):
         if data['title'] == data['description']:
             raise serializers.ValidationError("The title and description cannot be the same.")
+        return data
+
+
+
+
+
+# class MovieSerializer(serializers.Serializer):
+    # id = serializers.IntegerField(read_only=True)
+    # title = serializers.CharField()
+    # description = serializers.CharField()
+    # active = serializers.BooleanField()
+
+    # # This is the POST method for creating a new movie
+    # def create(self, validated_data):
+    #     # validated data is passed to create method of Movie model
+    #     return Movie.objects.create(**validated_data) # the ** operator unpacks the dictionary
+    
+    # # This is the PUT menthod for updating an existing movie
+    # def update(self, instance, validated_data):
+    #     # instance is the movie object to be updated, validated_data is the new data
+    #     instance.title = validated_data.get('title', instance.title)
+    #     instance.description = validated_data.get('description', instance.description)
+    #     instance.active = validated_data.get('active', instance.active)
+    #     instance.save()
+    #     return instance
+
+    # # Field level validation
+    # def validate_title(self, value):
+    #     if len(value) < 2:
+    #         raise serializers.ValidationError('The title must be at least 2 characters long.')
+    #     else:
+    #         return value
+    
+    # # object level validation
+    # def validate(self, data):
+    #     if data['title'] == data['description']:
+    #         raise serializers.ValidationError("The title and description cannot be the same.")
+    #     return data
--- udemy_movie_project/watchlist_app/models.py
+++ udemy_movie_project/watchlist_app/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Movie(models.Model): 
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=500)
+    description = models.CharField(default=True, max_length=200)
     active = models.BooleanField(default=True)
 
     def __str__(self):
