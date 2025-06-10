cc474ac962cbd55f383496cfa2082e45c32c2b83
Michelle <michelle@Michelles-MacBook-Air.local>
2025-06-10 11:14:03 -0400

    change media_type to have a choice dropdown in the admin panel

--- udemy_movie_project/watchlist_app/models.py
+++ udemy_movie_project/watchlist_app/models.py
@@ -9,12 +9,22 @@ class StreamPlatform(models.Model):
         return self.name
 
 class MediaType(models.Model): 
-    media_type = models.CharField(max_length=50, null=True, blank=True)
+    media_type_choices = [
+    ('Movie', 'Movie'),
+    ('TV Show', 'TV Show'),
+    ('Series', 'Series'),
+    ('Documentary', 'Documentary'),
+    ('Short Film', 'Short Film'),
+    ('Film', 'Film'),
+]
+    
+    media_type = models.CharField(choices=media_type_choices, max_length=50, null=True, blank=True)
     genre = models.CharField(max_length = 50, null=True, blank=True)
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
     active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
+    
 
     def __str__(self):
         return self.title
